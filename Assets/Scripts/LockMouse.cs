c438d776b6fef161a72b90a28a4eea3f8280fd01
Eric Lipschutz <selkcip@gmail.com>
2014-03-17 18:19:55 -0700

    More Stuff

--- Assets/Scripts/LockMouse.cs
+++ Assets/Scripts/LockMouse.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class LockMouse : MonoBehaviour {
 
+	public bool lockMouse = true;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,11 @@ public class LockMouse : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Screen.lockCursor = true;
+		if(lockMouse) {
+			Screen.lockCursor = true;
+		}
+		else {
+			Screen.lockCursor = false;
+		}
 	}
 }
