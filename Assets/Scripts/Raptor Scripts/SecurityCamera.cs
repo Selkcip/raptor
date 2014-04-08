68ef592aab59e8eb9f1bddf8afabcd746eea22ec
Nick Nguyen <niqnguye@ucsc.edu>
2014-04-07 17:20:00 -0700

    Security Cameras and Sentry Turrets
    yeah, whatever.

---
+++ Assets/Scripts/Raptor Scripts/SecurityCamera.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Holoville.HOTween;
+
+public class SecurityCamera : MonoBehaviour {
+
+	//Looking things
+	public float fov = 45f;
+	public float viewDis = 10f;
+	public float noticeTime = 2;
+
+	float curFov = 0;
+	float curViewDis = 0;
+	float alertFov = 180f;
+	float alertViewDis = 10;
+	Vector3 enemyDiff;
+	Vector3 enemyPos;
+	Vector3 enemyDir;
+	public float noticeTimer = 0;
+	public bool enemyVisible = false;
+	bool mentionEnemyVisible = false;
+	public bool enemySeen = false;
+
+	//Tweening things
+	private bool tweening = false;
+	private Transform player;
+	private Vector3 startingRotation;
+
+	private float rotationRange = 45f;//if you change this, you have to change the handle's rotation
+	public bool canRotate = true;
+
+	// Use this for initialization
+	void Start() {
+		player = GameObject.Find("Player").transform;
+		startingRotation = transform.localEulerAngles;
+
+		curFov = fov;
+		curViewDis = viewDis;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//searching
+		if(!enemyVisible && !tweening && canRotate) {
+			tweening = true;
+			ResetSearch();
+		}
+		else if(enemyVisible) {
+			if(canRotate) {
+				HOTween.Kill(transform);
+				tweening = false;
+				transform.LookAt(player.position);
+			}
+			Alarm.ActivateAlarms();
+		}
+
+		curFov = Mathf.Max(fov, curFov - 1.0f * Time.deltaTime);
+		curViewDis = 10f;//Mathf.Max(0.0001f, viewDis * lightLevel / maxLightLevel);//Mathf.Max(viewDis, curViewDis - 1.0f * Time.deltaTime);
+		if(Alarm.activated) {
+			curFov = alertFov;
+			//curViewDis = viewDis*2;
+		}
+
+		LookForEnemy();
+
+		Debug.DrawRay(transform.position, transform.forward, Color.magenta);
+	}
+
+	void LookForEnemy() {
+		enemyVisible = false;
+		RaptorInteraction player = GameObject.Find("Player").GetComponent<RaptorInteraction>();
+		if(player != null && player.health > 0) {
+			Transform enemyHead = Camera.main.transform;
+			if(enemyHead != null) {
+				enemyDiff = enemyHead.position - transform.position;
+				enemyDir.x = enemyDiff.x;
+				enemyDir.y = enemyDiff.y;
+				enemyDir.z = enemyDiff.z;
+				enemyDiff.Normalize();
+
+				if(Vector3.Dot(transform.forward, enemyDiff) >= 1.0f - (curFov / 2.0f) / 90.0f) {
+					RaycastHit hit;
+					if(Physics.Raycast(transform.position, enemyDir, out hit, curViewDis)) {
+						if(hit.collider.tag == "Player" || (hit.collider.transform.parent != null && hit.collider.transform.parent.tag == "Player")) {
+							noticeTimer += (1.0f - hit.distance / curViewDis) * Time.deltaTime;
+							if(noticeTimer >= noticeTime) {
+								enemyVisible = true;
+								enemySeen = true;
+								enemyPos = enemyHead.position;
+							}
+						}
+					}
+				}
+			}
+		}
+		//Replace this stuff with camera sounds
+		if(enemySeen) {
+			if(!mentionEnemyVisible) {
+				List<string> lines = new List<string>();
+				lines.Add("sweetjesusisthataraptor");
+				lines.Add("whatwasthat");
+				lines.Add("didyouseethat");
+				SoundManager.instance.Play3DSound((AudioClip)Resources.Load("Sounds/Raptor Sounds/enemies/Guard/" + lines[Random.Range(0, lines.Count - 1)]), SoundManager.SoundType.Dialogue, gameObject);
+				mentionEnemyVisible = true;
+			}
+		}
+		else {
+			if(mentionEnemyVisible) {
+				SoundManager.instance.Play3DSound((AudioClip)Resources.Load("Sounds/Raptor Sounds/enemies/Guard/wherediditgo"), SoundManager.SoundType.Dialogue, gameObject);
+				mentionEnemyVisible = false;
+			}
+		}
+	}
+
+	void ResetSearch() {
+		if(enemyVisible) {
+			tweening = false;
+			return;
+		}
+		noticeTimer = 0f;
+		HOTween.To(transform, 5.0f, new TweenParms().Prop("localRotation", startingRotation, false).OnComplete(SearchRight));
+	}
+
+	void SearchRight() {
+		if(enemyVisible) {
+			tweening = false;
+			return;
+		}
+		HOTween.To(transform, 5.0f, new TweenParms().Prop("localRotation", new Vector3(0f, rotationRange, 0f), true).OnComplete(SearchLeft));
+	}
+
+	void SearchLeft() {
+		if(enemyVisible) {
+			tweening = false;
+			return;
+		}
+		HOTween.To(transform, 5.0f, new TweenParms().Prop("localRotation", new Vector3(0f, -rotationRange, 0f), true).OnComplete(SearchRight));
+	}
+}
+
