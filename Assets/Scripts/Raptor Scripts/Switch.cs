f24980c6bda50b437f2e581b854c8aa14a5578bf
Eric Lipschutz <selkcip@gmail.com>
2014-04-15 15:33:58 -0700

    Light Switch

---
+++ Assets/Scripts/Raptor Scripts/Switch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : Triggerable {
+	float toggleTime = 0.1f;
+	float toggleTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//base.Update();
+	}
+
+	public void Use(GameObject user) {
+		if(toggleTimer <= 0) {
+			isTriggered = !isTriggered;
+			toggleTimer = toggleTime;
+		}
+		else {
+			toggleTimer -= Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/Raptor Scripts/TriggerableLight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerableLight : Triggerable {
+
+	void Start () {
+		activateOnStateChange = true;
+	}
+
+	void Activate(bool triggered) {
+		if(light != null) {
+			light.enabled = triggered;
+		}
+	}
+}
