1c215f4b95d20053a0788257376734ec53a8e4a7
Eric Lipschutz <selkcip@gmail.com>
2014-04-19 22:07:13 -0700

    Scene transition

---
+++ Assets/Scripts/Raptor Scripts/TaggedTriggerable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+/*[System.Serializable]
+public class Tag {
+	public string name = "tag";
+	public bool selected = false;
+
+	public Tag(string name) {
+		this.name = name;
+	}
+}
+
+[CustomEditor(typeof(Tag))]
+public class TagEditor : Editor {
+}
+
+[ExecuteInEditMode()]*/
+public class TaggedTriggerable : Triggerable {
+	//public List<Tag> tags;
+	public List<string> tags = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		/*if(Application.isEditor) {
+			tags = new List<Tag>();
+			foreach(string tag in UnityEditorInternal.InternalEditorUtility.tags) {
+				tags.Add(new Tag(tag));
+			}
+		}*/
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(tags.Contains(other.tag) || tags.Contains(other.transform.parent.tag)) {
+			isTriggered = true;
+		}
+	}
+}
