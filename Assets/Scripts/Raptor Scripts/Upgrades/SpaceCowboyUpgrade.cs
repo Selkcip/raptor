a92e1ca13551fa9e82d3931c40b704fa92c82d17
Eric Lipschutz <selkcip@gmail.com>
2014-04-23 15:01:47 -0700

    Space cowboy upgrade

---
+++ Assets/Scripts/Raptor Scripts/Upgrades/SpaceCowboyUpgrade.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceCowboyUpgrade : MonoBehaviour {
+
+	public override void Apply(RaptorInteraction player) {
+		//Give the player a space cowboy
+	}
+}
