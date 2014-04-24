5c7eae3a0ed9109fdd924b79cc9561a4d54ba463
ckhamash <ckhamash@ucsc.edu>
2014-04-23 19:14:00 -0700

    Can now easily add spacey features! Slightly buggy cargo lane implemented

---
+++ Assets/Scripts/CargoShip.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CargoShip : MonoBehaviour {
+	public float baseForce;
+    public float maxRotateAngle;
+    public float detectionRange;
+	public float fireAngle;
+
+    public GameObject bullet;
+    public float bulletSpeed;
+	float reload = 0;
+	public float reloadTime;
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start() {
+        player = GameObject.Find("PlayerShip");
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (isPlayerSpotted()) {
+			Shoot();
+		}
+
+		if (reload > 0) {
+			reload -= Time.deltaTime;
+		}
+	}
+
+	public bool isPlayerSpotted()
+	{
+		Vector3 targetDirection = player.transform.position - transform.position;
+		float distance = targetDirection.magnitude;
+		return !player.GetComponent<PlayerShipController>().isCloaked && distance <= detectionRange;
+	}
+
+	void Shoot() {
+		if (reload <= 0) {
+			Vector3 targetDirection = player.transform.position - transform.position;
+			targetDirection += (Vector3)(player.rigidbody2D.velocity - rigidbody2D.velocity) * targetDirection.magnitude / bulletSpeed;
+
+            //Debug.DrawRay(transform.position, targetDirection);
+
+			GameObject shot = (GameObject)Instantiate(bullet, transform.position, Quaternion.FromToRotation(Vector3.up, targetDirection));
+			shot.rigidbody2D.velocity = rigidbody2D.velocity + (Vector2)(shot.transform.up * bulletSpeed);
+			reload = reloadTime;
+		}
+	}
+
+    void OnCollisionEnter2D(Collision2D col) { 
+        // detect if hit by a shot
+    }
+}
--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 // attach this to the player's ship object
+
 public class LevelSelector : MonoBehaviour {
 
 	public float despawnRadius;
@@ -18,17 +18,25 @@ public class LevelSelector : MonoBehaviour {
     public bool isPlayerSpotted;
     public Vector3 lastDetectedLocation;
 
-	ArrayList ships; // list of active ships in the scene (besides player)
+    ArrayList features; // list of spawned space features
+
+    ArrayList cargoShips; // list of active cargo ships
+	ArrayList policeShips; // list of active police ships in the scene
 
     ArrayList debris; // stationary background space debris
 
 	// Use this for initialization
     void Start()
     {
-		ships = new ArrayList();
+        cargoShips = new ArrayList();
+		policeShips = new ArrayList();
         debris = new ArrayList();
+        features = new ArrayList();
         isPlayerSpotted = false;
         lastDetectedLocation = transform.position;
+
+        CargoLane cargoLane = new CargoLane(new Vector2(10, 0), new Vector2(1, 1), new Vector2(10, 15), 10, new Vector2(11, 12));
+        features.Add(cargoLane);
 	}
 
 	public void Load(){
@@ -38,9 +46,25 @@ public class LevelSelector : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
+        UpdateShips();
+
+        UpdateBackground();
+        foreach (SpaceyFeature feature in features) { 
+            switch(feature.type) {
+                case FeatureType.CargoLane:
+                    UpdateCargoLane((CargoLane)feature);
+                    break;
+                // to add additional features:
+                // at the bottom of the script add an enum type to FeatureType, 
+                // a class implementing SpaceyFeature, and an update function for it
+            }
+        }
+	}
+
+    void UpdateShips() {
         isPlayerSpotted = false;
 		ArrayList toBeRemoved = new ArrayList();
-		foreach (GameObject ship in ships) {
+		foreach (GameObject ship in policeShips) {
             if (Vector3.Distance(transform.position, ship.transform.position) > despawnRadius)
                 toBeRemoved.Add(ship);
             else if (ship.GetComponent<PoliceShip>().isPlayerSpotted()) {
@@ -49,29 +73,52 @@ public class LevelSelector : MonoBehaviour {
             }
 		}
 		foreach (GameObject ship in toBeRemoved) {
-			ships.Remove(ship);
+			policeShips.Remove(ship);
 			Destroy(ship);
 		}
 
-		while (ships.Count < maxShips) {
+        toBeRemoved.Clear();
+        foreach (GameObject ship in cargoShips)
+        {
+            if (Vector3.Distance(transform.position, ship.transform.position) > despawnRadius)
+                toBeRemoved.Add(ship);
+            else if (ship.GetComponent<CargoShip>().isPlayerSpotted())
+            {
+                isPlayerSpotted = true;
+                lastDetectedLocation = transform.position;
+            }
+        }
+
+        foreach (GameObject ship in toBeRemoved)
+        {
+            cargoShips.Remove(ship);
+            Destroy(ship);
+        }
+
+		/*while (policeShips.Count < maxShips) {
 			Vector2 position = Random.insideUnitCircle.normalized * spawnRadius;
 			float rotation = Random.value * 360;
 			GameObject ship = (GameObject)Instantiate(policeShip, transform.position + (Vector3)position, Quaternion.Euler(new Vector3(0, 0, rotation)));
-			ships.Add(ship);
-		}
+			policeShips.Add(ship);
+		}*/
+    }
 
-        toBeRemoved = new ArrayList();
+    void UpdateBackground() {
+
+        ArrayList toBeRemoved = new ArrayList();
         foreach (GameObject derb in debris)
             if (Vector3.Distance(transform.position, derb.transform.position) > despawnRadius)
                 toBeRemoved.Add(derb);
 
-        foreach (GameObject derb in toBeRemoved) {
+        foreach (GameObject derb in toBeRemoved)
+        {
             debris.Remove(derb);
             Destroy(derb);
         }
 
         // background debris
-        if (debris.Count < 1) {
+        if (debris.Count < 1)
+        {
             // place one near center if none yet
             Vector2 position = Random.insideUnitCircle.normalized * debrisRadius.x / 4;
             float z = Random.value * (debrisDepth.y - debrisDepth.x) + debrisDepth.x; // random depth
@@ -82,36 +129,112 @@ public class LevelSelector : MonoBehaviour {
         ArrayList newDerbs = new ArrayList(debris);
 
         //while (newDerbs.Count > 0) {
-            ArrayList toBeAdded = new ArrayList();
-            foreach (GameObject derb in newDerbs) {
-                Vector2 position = (Vector2)derb.transform.position + Random.insideUnitCircle.normalized * (debrisRadius.x + (debrisRadius.x - debrisRadius.y) * Random.value);
-                bool add = true;
-                if (((Vector2)transform.position - position).magnitude > despawnRadius) { 
+        ArrayList toBeAdded = new ArrayList();
+        foreach (GameObject derb in newDerbs)
+        {
+            Vector2 position = (Vector2)derb.transform.position + Random.insideUnitCircle.normalized * (debrisRadius.x + (debrisRadius.x - debrisRadius.y) * Random.value);
+            bool add = true;
+            if (((Vector2)transform.position - position).magnitude > despawnRadius)
+            {
+                add = false;
+                continue;
+            }
+            foreach (GameObject oldDerb in debris)
+                if (((Vector2)oldDerb.transform.position - position).magnitude < debrisRadius.x)
+                {
                     add = false;
-                    continue;
+                    break;
                 }
-                foreach (GameObject oldDerb in debris)
-                    if (((Vector2)oldDerb.transform.position - position).magnitude < debrisRadius.x) { 
-                        add = false;
-                        break;
-                    }
-                foreach (GameObject oldDerb in toBeAdded)
-                    if (((Vector2)oldDerb.transform.position - position).magnitude < debrisRadius.x) { 
-                        add = false;
-                        break;
-                    }
-                if (add) {
-                    float z = Random.value * (debrisDepth.y - debrisDepth.x) + debrisDepth.x; // random depth
-                    GameObject newDerb = (GameObject)Instantiate(debrisObject, new Vector3(position.x, position.y, z), Quaternion.identity);
-                    toBeAdded.Add(newDerb);
+            foreach (GameObject oldDerb in toBeAdded)
+                if (((Vector2)oldDerb.transform.position - position).magnitude < debrisRadius.x)
+                {
+                    add = false;
+                    break;
                 }
+            if (add)
+            {
+                float z = Random.value * (debrisDepth.y - debrisDepth.x) + debrisDepth.x; // random depth
+                GameObject newDerb = (GameObject)Instantiate(debrisObject, new Vector3(position.x, position.y, z), Quaternion.identity);
+                toBeAdded.Add(newDerb);
             }
-            newDerbs.Clear();
-            print(newDerbs.Count);
-            foreach (GameObject derb in toBeAdded) {
-                newDerbs.Add(derb);
-                debris.Add(derb);
-            }
+        }
+        newDerbs.Clear();
+        foreach (GameObject derb in toBeAdded)
+        {
+            newDerbs.Add(derb);
+            debris.Add(derb);
+        }
         //}
-	}
+    }
+
+    void UpdateCargoLane(CargoLane lane) {
+        // update location of lane to spot closest to player along direction
+        Vector2 distance = (Vector2)transform.position - lane.location;
+        Vector2 closest = Vector3.Project(distance, lane.direction);
+        lane.location += closest;
+
+        // if within spawnradius spawn ships
+        distance = (Vector2)transform.position - lane.location;
+        if (distance.magnitude < spawnRadius) { 
+            // get a spawn location
+            float spawnWidth = Random.value * lane.width - lane.width * 2;
+            float spawnDistance = Mathf.Sqrt(Mathf.Pow(spawnRadius, 2) - Mathf.Pow(distance.magnitude + spawnWidth, 2));
+            Vector2 spawnOrigin = lane.location - lane.direction.normalized * spawnDistance;
+
+            // check if it is not near an already spawned cargoship, then spawn
+            bool spawn = true;
+            foreach (GameObject ship in cargoShips)
+                if (Vector2.Distance(ship.transform.position, spawnOrigin) < lane.nextSpawnDistance) {
+                    spawn = false; 
+                    break;
+                }
+
+            if (spawn) { 
+                GameObject newShip = (GameObject)Instantiate(cargoShip, spawnOrigin, Quaternion.FromToRotation(Vector2.up, lane.direction));
+                newShip.rigidbody2D.velocity = (Vector2)newShip.transform.up * (Random.value * lane.initialSpeed.x + (lane.initialSpeed.y - lane.initialSpeed.x));
+                cargoShips.Add(newShip);
+                lane.CalculateNextSpawnDistance();
+            }
+        }
+        // else do nothing
+    }
 }
+
+public class SpaceyFeature {
+    public Vector2 location;
+    public FeatureType type;
+
+    public SpaceyFeature()
+    { }
+
+    public SpaceyFeature(Vector2 location) {
+        this.location = location;
+        type = FeatureType.Empty;
+    }
+}
+
+public class CargoLane : SpaceyFeature {
+    public Vector2 direction;
+    public Vector2 shipSpawnDistance; // min/max. Time in seconds until next ship spawns
+    public float width;
+    public Vector2 initialSpeed; // min/max. Velocity of ships spawned by this lane
+
+    public float nextSpawnDistance;
+
+    public CargoLane(Vector2 location, Vector2 direction, Vector2 spawnDistance, float width, Vector2 initialSpeed) {
+        this.location = location;
+        this.direction = direction;
+        shipSpawnDistance = spawnDistance;
+        this.width = width;
+        this.initialSpeed = initialSpeed;
+        type = FeatureType.CargoLane;
+        CalculateNextSpawnDistance();
+    }
+
+    public void CalculateNextSpawnDistance() {
+        nextSpawnDistance = shipSpawnDistance.x + Random.value * (shipSpawnDistance.y - shipSpawnDistance.x);
+    }
+}
+
+// enum for different feature types
+public enum FeatureType { Empty, CargoLane, UpgradeShip, SpaceStation }
