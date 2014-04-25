c08e92faace47d38c710bb4ea5fa58d0e9aadd3a
Eric Lipschutz <selkcip@gmail.com>
2014-04-24 22:23:24 -0700

    More saving

--- Assets/Scripts/Raptor Scripts/Menu Scripts/GameSaver.cs
+++ Assets/Scripts/Raptor Scripts/Menu Scripts/GameSaver.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 
+public class GameSave : Dictionary<string, string> {
+
+}
+
 public class GameSaver : MonoBehaviour {
-	public static Dictionary<string, string> OpenSave(string saveName) {
+	public static GameSave currentSave;
+
+	public static GameSave Open(string saveName) {
 		string save = PlayerPrefs.GetString(saveName);
 		string[] pairs = save.Split(new char[]{','});
-		Dictionary<string, string> values = new Dictionary<string, string>();
+		GameSave values = new GameSave();
 		foreach(string pair in pairs) {
 			string[] keyValue = pair.Split(new char[] { '=' });
 			if(keyValue.Length > 1) {
@@ -19,7 +25,7 @@ public class GameSaver : MonoBehaviour {
 		return values;
 	}
 
-	public static void CloseSave(string saveName, Dictionary<string, string> save){
+	public static void Save(string saveName, GameSave save) {
 		string saveString = "";
 		bool first = true;
 		foreach(KeyValuePair<string, string> pair in save) {
@@ -34,51 +40,88 @@ public class GameSaver : MonoBehaviour {
 		PlayerPrefs.SetString(saveName, saveString);
 	}
 
-	public static void SaveValue(Dictionary<string, string> save, string key, string value) {
-		save.Remove(key);
-		save.Add(key, value);
+	public static void SetValue(string key, string value) {
+		currentSave.Remove(key);
+		currentSave.Add(key, value);
 	}
 
-	public static void SaveValue(Dictionary<string, string> save, string key, int value) {
-		save.Remove(key);
-		save.Add(key, value.ToString());
+	public static void SetValue(string key, int value) {
+		currentSave.Remove(key);
+		currentSave.Add(key, value.ToString());
 	}
 
-	public static void SaveValue(Dictionary<string, string> save, string key, float value) {
-		save.Remove(key);
-		save.Add(key, value.ToString());
+	public static void SetValue(string key, float value) {
+		currentSave.Remove(key);
+		currentSave.Add(key, value.ToString());
 	}
 
-	public static void SaveValue(Dictionary<string, string> save, string key, bool value) {
-		save.Remove(key);
-		save.Add(key, value.ToString());
+	public static void SetValue(string key, bool value) {
+		currentSave.Remove(key);
+		currentSave.Add(key, value.ToString());
 	}
 
-	public static void getValue(Dictionary<string, string> save, string key, out string value) {
-		save.TryGetValue(key, out value);
+	public static void GetValue(string key, out string value) {
+		currentSave.TryGetValue(key, out value);
 	}
 
-	public static void getValue(Dictionary<string, string> save, string key, out int value) {
+	public static void GetValue(string key, out int value) {
 		string stringValue;
-		save.TryGetValue(key, out stringValue);
+		currentSave.TryGetValue(key, out stringValue);
 		if(!int.TryParse(stringValue, out value)) {
 			value = 0;
 		}
 	}
 
-	public static void getValue(Dictionary<string, string> save, string key, out float value) {
+	public static void GetValue(string key, out float value) {
 		string stringValue;
-		save.TryGetValue(key, out stringValue);
+		currentSave.TryGetValue(key, out stringValue);
 		if(!float.TryParse(stringValue, out value)) {
 			value = 0;
 		}
 	}
 
-	public static void getValue(Dictionary<string, string> save, string key, out bool value) {
+	public static void GetValue(string key, out bool value) {
 		string stringValue;
-		save.TryGetValue(key, out stringValue);
+		currentSave.TryGetValue(key, out stringValue);
 		if(!bool.TryParse(stringValue, out value)) {
 			value = false;
 		}
+	}
+
+	public static void LoadGame(string saveName) {
+		currentSave = Open(saveName);
+		if(currentSave.Count <= 0) {
+			CreateGame(saveName);
+		}
+		float notoriety, map;
+		int money;
+		string name;
+		GetValue("notoriety", out notoriety);
+		GetValue("map", out map);
+		GetValue("money", out money);
+		GetValue("name", out name);
+
+		print(notoriety + " " + map + " " + money + " " + name);
+
+		RaptorInteraction.notoriety = notoriety;
+		RaptorInteraction.mapAmountAcquired = map;
+		RaptorInteraction.money = money;
+		RaptorInteraction.name = name;
+	}
+
+	public static void CreateGame(string saveName) {
+		SetValue("notoriety", 0f);
+		SetValue("money", 0);
+		SetValue("map", 0f);
+		SetValue("name", "Click to enter name here");
+		Save(saveName, currentSave);
+	}
+
+	public static void SaveGame(string saveName) {
+		SetValue("notoriety", RaptorInteraction.notoriety);
+		SetValue("money", RaptorInteraction.money);
+		SetValue("map", RaptorInteraction.mapAmountAcquired);
+		SetValue("name", RaptorInteraction.name);
+		Save(saveName, currentSave);
 	}
 }
