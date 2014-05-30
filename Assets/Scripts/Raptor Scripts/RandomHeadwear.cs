a5906a56aaa6cc3d3d8fefa656b66ed8b8fec19c
Eric Lipschutz <selkcip@gmail.com>
2014-05-30 01:30:55 -0700

    Added some more cosmetic items to the NPCs

---
+++ Assets/Scripts/Raptor Scripts/RandomHeadwear.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomHeadwear : MonoBehaviour {
+	public List<GameObject> headWear = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		int keep = Random.Range(0, headWear.Count);
+		for(int i = 0; i < headWear.Count; i++) {
+			if(i != keep) {
+				headWear[i].SetActive(false);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
